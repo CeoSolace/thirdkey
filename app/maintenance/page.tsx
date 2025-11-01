9ceb32277b2e13d9ccb7a6c8a78f1552c64572fa
Solace <monkdev9@gmail.com>
2025-11-01 22:34:44 +0000

    Create page.tsx

---
+++ app/maintenance/page.tsx
@@ -0,0 +1,10 @@
+export default function MaintenancePage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold mb-4">We'll be back soon!</h1>
+        <p>ThirdKey is currently under maintenance. We'll be back shortly.</p>
+      </div>
+    </div>
+  );
+}
